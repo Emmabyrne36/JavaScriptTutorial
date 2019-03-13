eda791cc10e6d9e0ae9081757a93f11b15385f7d
Emmabyrne36 <emmabyrne36@gmail.com>
2019-03-13 15:46:39 +0000

    Completed animations

--- part2_Browser.js
+++ part2_Browser.js
@@ -300,3 +300,82 @@ function editDiv() {
 }
 
 editDiv();
+
+function animatePlane() {
+  let jet = document.getElementById('flyjet');
+  jet.onclick = function () {
+    let ended = false;
+
+    flyjet.addEventListener('transitioned', function () {
+      if (!ended) {
+        ended = true;
+        console.log('Done!');
+      }
+    });
+
+    flyjet.classList.add('growing');
+  };
+}
+
+animatePlane();
+
+function showCircle(cx, cy, radius) {
+  console.log('in the method');
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  setTimeout(() => {
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+  }, 0);
+}
+
+function bouncingBall() {
+  function makeEaseOut(timing) {
+    return function (timeFraction) {
+      return 1 - timing(1 - timeFraction);
+    };
+  }
+
+  function bounce(timeFraction) {
+    for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+      if (timeFraction >= (7 - 4 * a) / 11) {
+        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
+      }
+    }
+  }
+
+  function quad(timeFraction) {
+    return Math.pow(timeFraction, 2);
+  }
+
+  let ball = document.getElementById('bounceBall');
+  ball.onclick = function () {    
+    let field = document.getElementById('bounceField');
+    let height = field.clientHeight - ball.clientHeight;
+    let width = 100;
+
+    animate({
+      duration: 2000,
+      timing: makeEaseOut(bounce),
+      draw(progress) {
+        ball.style.top = height * progress + 'px';
+      }
+    });
+
+    animate({
+      duration: 2000,
+      timing: makeEaseOut(quad),
+      draw: function (progress) {
+        ball.style.left = width * progress + 'px';
+      }
+    });
+  };
+}
+
+bouncingBall();
